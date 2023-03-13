3f4aebd9b517167a5542b2b5651b990e2bd1eb7d
Kaue Oliveira <kauelucas@terra.com.br>
2023-03-12 21:40:23 -0300

    added year test

--- main.py
+++ main.py
@@ -1,32 +1,65 @@
-#main.py
+# main.py
 
-from requests_html import HTMLSession
+from datetime import datetime
+from bs4 import BeautifulSoup
+import requests
+import re
+import sys
 
-def get_reviews(url): 
-    sess = HTMLSession()   
-    reviews_list = {"reviews":[], 'url':url}
-    res = sess.get(url)
-    html = res.html
-    reviews = html.find('.user_reviews', first=True).find('.review')
+
+def get_reviews(url, release_year, cerimony_year):
+    reviews_list = {"reviews": [
+    ], 'url': 'url'}
+    session = requests.Session()
+    session.headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.69'}
+    res = session.get(url)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    date_str = soup.find("span", class_='release_date').contents[3].get_text()
+    date_obj = date_obj = datetime.strptime(date_str, '%B %d, %Y').date()
+    if (release_year != date_obj.year):
+        raise Exception("Anos diferentes - "+url)
+    reviews = soup.find("div", class_='user_reviews').find_all(
+        'div', class_='review')
     count = 0
     for review in reviews:
-        count += 1
-        print(review.find(".date", first=True).text)
-    next_page = html.find('.flipper.next', first=True)
+        review_date_str = review.find('span', class_='date').get_text()
+        review_date_obj = datetime.strptime(
+            review_date_str, '%b %d, %Y').date()
+        if (cerimony_year == review_date_obj.year):
+            count += 1
+    next_page = soup.find('div', class_='page_flipper').find(
+        'span', class_='flipper next').find('a')
     if next_page:
-        try:
-            next_page_url = next_page.absolute_links.pop() 
-            reviews_list['reviews'].extend(get_reviews(next_page_url)['reviews'])
-        except KeyError:
-            print(KeyError)
-    print(count)
+        next_page_url = 'https://www.metacritic.com' + next_page['href']
+        reviews_list['reviews'].extend(get_reviews(
+            next_page_url, release_year, cerimony_year)['reviews'])
     return reviews_list
-
-url = 'https://www.metacritic.com/movie/whiplash/user-reviews?sort-by=date&num_items=100'
-
-
-
 
 
 if __name__ == '__main__':
-    get_reviews(url)
+    second_attemp = False
+
+    filename = "log.txt"
+    file_obj = open(filename, "w")
+    sys.stdout = file_obj
+
+    film = '@\'Whiplash'
+    film_string = film.lower().replace(" ", "-")
+    film_string = re.sub(r'[^a-zA-Z0-9\-]+', '', film_string)
+    try:
+        get_reviews(
+            f'https://www.metacritic.com/movie/{film_string}/user-reviews?sort-by=date&num_items=100', 2014, 2015)
+        print(film_string + " adicionado com sucesso")
+    except AttributeError as e:
+        try:
+            print(film_string+' não encontrado, tentando adicionar o ano ao final')
+            film_string = film_string + "-" + '2014'
+            get_reviews(
+                f'https://www.metacritic.com/movie/{film_string}/user-reviews?sort-by=date&num_items=100', 2014, 2015)
+        except Exception as e:
+            print(film_string+' não encontrado')
+    except Exception as e:
+        print(film_string+' com ano incorreto, tentando novamente')
+    finally:
+        file_obj.close()
