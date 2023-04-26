f959e6931d319f3513a66ffd60a6d8ac25701f73
Kaue Oliveira <kauelucas@terra.com.br>
2023-04-25 21:59:24 -0300

    scatter plots

--- new_features.py
+++ new_features.py
@@ -8,24 +8,51 @@ import ast
 def plotHistogram():
     with open('oscar_movies_statistics.json', 'r') as json_file:
         movies = json.load(json_file)
-        # Create a figure and three subplots
         fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(10, 5))
 
         medias = []
         medianas = []
-        modas = []
+        stdev = []
 
         for movie in movies:
             medias.append(movies[movie]['critic-mean'])
             medianas.append(movies[movie]['critic-median'])
-            modas.append(movies[movie]['critic-stdev'])
+            stdev.append(movies[movie]['critic-stdev'])
 
-        # Plot the histograms
         ax1.hist(medias, bins=20, color='blue')
         ax2.hist(medianas, bins=20, color='green')
-        ax3.hist(modas, bins=20, color='red')
+        ax3.hist(stdev, bins=20, color='red')
 
-        # Add titles and axis labels
+        ax1.set_title('Média')
+        ax2.set_title('Mediana')
+        ax3.set_title('Desvio padrão')
+        ax1.set_xlabel('Valores')
+        ax2.set_xlabel('Valores')
+        ax3.set_xlabel('Valores')
+        ax1.set_ylabel('Frequência')
+
+        plt.show()
+
+
+def plotHistogramByClass(class_):
+    with open('oscar_movies_statistics.json', 'r') as json_file:
+        movies = json.load(json_file)
+        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(10, 5))
+
+        medias = []
+        medianas = []
+        stdev = []
+
+        for movie in movies:
+            if movies[movie]['winner'] == class_:
+                medias.append(movies[movie]['critic-mean'])
+                medianas.append(movies[movie]['critic-median'])
+                stdev.append(movies[movie]['critic-stdev'])
+
+        ax1.hist(medias, bins=20, color='blue')
+        ax2.hist(medianas, bins=20, color='green')
+        ax3.hist(stdev, bins=20, color='red')
+
         ax1.set_title('Média')
         ax2.set_title('Mediana')
         ax3.set_title('Desvio padrão')
@@ -37,36 +64,70 @@ def plotHistogram():
         plt.show()
 
 
-def plotHistogramByClass(class_):
-    with open('oscar_movies_statistics.json', 'r') as json_file:
+def plotScatter():
+    with open('golden_globe_movies_statistics.json', 'r') as json_file:
         movies = json.load(json_file)
-        # Create a figure and three subplots
-        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(10, 5))
+        fig, (ax1, ax2, ax3) = plt.subplots(
+            3, 1, figsize=(5, 10))
 
-        medias = []
-        medianas = []
-        modas = []
+        medias_critic = ([], [])
+        medianas_critic = ([], [])
+        stdev_critic = ([], [])
+
+        medias_user = ([], [])
+        medianas_user = ([], [])
+        stdev_user = ([], [])
 
         for movie in movies:
-            if movies[movie]['winner'] == class_:
-                medias.append(movies[movie]['critic-mean'])
-                medianas.append(movies[movie]['critic-median'])
-                modas.append(movies[movie]['critic-stdev'])
+            if movies[movie]['winner'] == "True":
+                medias_critic[0].append(movies[movie]['critic-mean'])
+                medianas_critic[0].append(movies[movie]['critic-median'])
+                stdev_critic[0].append(movies[movie]['critic-stdev'])
 
-        # Plot the histograms
-        ax1.hist(medias, bins=20, color='blue')
-        ax2.hist(medianas, bins=20, color='green')
-        ax3.hist(modas, bins=20, color='red')
+                medias_user[0].append(movies[movie]['user-mean'])
+                medianas_user[0].append(movies[movie]['user-median'])
+                stdev_user[0].append(movies[movie]['user-stdev'])
+            else:
+                medias_critic[1].append(movies[movie]['critic-mean'])
+                medianas_critic[1].append(movies[movie]['critic-median'])
+                stdev_critic[1].append(movies[movie]['critic-stdev'])
 
-        # Add titles and axis labels
-        ax1.set_title('Média')
-        ax2.set_title('Mediana')
-        ax3.set_title('Desvio padrão')
-        ax1.set_xlabel('Valores')
-        ax2.set_xlabel('Valores')
-        ax3.set_xlabel('Valores')
-        ax1.set_ylabel('Frequência')
-        # Show the plot
+                medias_user[1].append(movies[movie]['user-mean'])
+                medianas_user[1].append(movies[movie]['user-median'])
+                stdev_user[1].append(movies[movie]['user-stdev'])
+
+        ax1.scatter(medias_critic[0], medianas_critic[0], label='Ganhador')
+        ax1.scatter(medias_critic[1], medianas_critic[1], label="Perdedor")
+
+        ax2.scatter(medias_critic[0], stdev_critic[0])
+        ax2.scatter(medias_critic[1], stdev_critic[1])
+
+        ax3.scatter(medianas_critic[0], stdev_critic[0])
+        ax3.scatter(medianas_critic[1], stdev_critic[1])
+
+        # ax1.scatter(medias_user[0], medianas_user[0], label='Ganhador')
+        # ax1.scatter(medias_user[1], medianas_user[1], label="Perdedor")
+
+        # ax2.scatter(medias_user[0], stdev_user[0])
+        # ax2.scatter(medias_user[1], stdev_user[1])
+
+        # ax3.scatter(medianas_user[0], stdev_user[0])
+        # ax3.scatter(medianas_user[1], stdev_user[1])
+
+        ax1.legend()
+
+        ax1.set_title('Críticos')
+        # ax1.set_title('Usuários')
+
+        ax1.set_xlabel('Média')
+        ax1.set_ylabel('Mediana')
+
+        ax2.set_xlabel('Média')
+        ax2.set_ylabel('Desvio Padrão')
+
+        ax3.set_xlabel('Mediana')
+        ax3.set_ylabel('Desvio Padrão')
+
         plt.show()
 
 
@@ -90,4 +151,4 @@ def generateFeatures():
 
 
 if __name__ == '__main__':
-    plotHistogramByClass("True")
+    plotScatter()
